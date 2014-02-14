aa0b59caec363e6facafb3d4b452aac61e747dd2
huangwei <huangweilook@21cn.com>
2014-02-15 00:29:09 +0800

    none

---
+++ javascripts/config.js
@@ -0,0 +1,6 @@
+var disqus_shortname = 'sniperhw';
+var hostbase = 'http://sniperHW.github.io';
+var githubname = 'sniperHW';
+var repos = 'sniperHW.github.io';
+var sitetitle = 'sniperHW';
+var rss = '';
