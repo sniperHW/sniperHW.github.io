5d1762658afff356a1b71822cce8609f0401a66e
huangwei <huangweilook@21cn.com>
2014-02-15 12:22:37 +0800

    none

--- javascripts/getmd.js
+++ javascripts/getmd.js
@@ -134,9 +134,10 @@ function chktakinglonger(){
 }
 
 function showpost(path){
-	alert(path)
 	var url = location.protocol + '//' + location.hostname + '/md/' + path.substr(1).replace(/\//g, '-');
+	alert(url)
 	document.title = decodeUtf8(path.substr(1).split('/')[path.substr(1).split('/').length-1].replace(/_/g, ' ')) + ' - '+sitetitle;
+	alert(document.title)
 	pdate = path.substr(1).split('/')[0]+'-'+path.substr(1).split('/')[1]+'-'+path.substr(1).split('/')[2];
 	loadXMLDoc(url);
 }
